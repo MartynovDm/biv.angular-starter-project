a308dc82b42e0eaba21321d5610626c92510b2b7
Martynov Dmitrii <dmartynov@bivgroup.ru>
2025-01-21 10:53:27 +0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'policy',
+    loadChildren: () => import('../app/pages/policy/policy.module').then(m => m.PolicyModule),
+  },
+  { path: '**', redirectTo: 'policy' },
+];
+
+export const public_routes: Routes = [];
